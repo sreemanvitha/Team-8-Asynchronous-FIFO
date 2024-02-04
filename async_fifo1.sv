b91436f6af1af14dce82ee4fbb3464ebbd5f1d2a
manideep333 <117485683+manideep333@users.noreply.github.com>
2024-02-04 09:57:08 -0800

    Add files via upload

---
+++ async_fifo1.sv
@@ -0,0 +1,25 @@
+module async_fifo1
+#(
+  parameter DSIZE = 8,
+  parameter ASIZE = 8
+ )
+(
+  input   winc, wclk, wrst_n,//winc write enable signal
+  input   rinc, rclk, rrst_n,//rinc read enable signal
+  input   [DSIZE-1:0] wdata,
+
+  output  [DSIZE-1:0] rdata,
+  output  wfull,
+  output  rempty
+);
+
+  wire [ASIZE-1:0] waddr, raddr;
+  wire [ASIZE:0] wptr, rptr, wq2_rptr, rq2_wptr;
+
+  sync_r2w sync_r2w (.*);
+  sync_w2r sync_w2r (.*);
+  fifomem #(DSIZE, ASIZE) fifomem (.*);
+  rptr_empty #(ASIZE) rptr_empty (.*);
+  wptr_full #(ASIZE) wptr_full (.*);
+
+endmodule
