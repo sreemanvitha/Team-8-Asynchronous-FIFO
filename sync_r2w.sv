fdcc9e75c14ff7135cfda06d112127f55149359d
pruthvimm <52813293+pruthvimm@users.noreply.github.com>
2024-02-04 09:41:58 -0800

    Add files via upload

---
+++ sync_r2w.sv
@@ -0,0 +1,17 @@
+module sync_r2w
+#(
+  parameter ADDRSIZE = 8
+)
+(
+  input   wclk, wrst_n,
+  input   [ADDRSIZE:0] rptr,
+  output reg  [ADDRSIZE:0] wq2_rptr//readpointer with write side
+);
+
+  reg [ADDRSIZE:0] wq1_rptr;
+
+  always_ff @(posedge wclk or negedge wrst_n)
+    if (!wrst_n) {wq2_rptr,wq1_rptr} <= 0;
+    else {wq2_rptr,wq1_rptr} <= {wq1_rptr,rptr};
+
+endmodule
