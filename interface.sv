b038e9acbcf22bc4d4352aa12db0b5cc6fde6d7b
bhavyareddygandeti <126542467+bhavyareddygandeti@users.noreply.github.com>
2024-02-25 23:09:35 -0800

    Add files via upload

--- interface.sv
+++ interface.sv
@@ -1,17 +1,20 @@
-interface fifo_intf #(parameter DSIZE = 8,
-                      parameter ASIZE = 8
-                     ) 
-                     (input logic wclk, rclk, wrst_n, rrst_n);
-  
-  
-  
-  //declaring the signals
-  logic  winc;  
-  logic  rinc; 
-  logic  [DSIZE-1:0] wdata;
-  logic  [DSIZE-1:0] rdata;
-  logic  wfull;
-  logic  rempty;
-  int ID;
-  
+//------------------------------------------------------
+//----------ASYNCHRONOUS FIFO INTERFACE-----------------
+//------------------------------------------------------
+
+//write interface
+interface write_if(input wclk, input wrst_n);
+   logic [8-1:0] wdata;
+   logic winc;
+   logic wfull;
+   int Burst_ID;
 endinterface
+
+
+//Read Interface
+interface read_if(input rclk, input rrst_n);
+   logic [8-1:0] rdata;
+   logic rinc;
+   logic rempty;
+endinterface
+
